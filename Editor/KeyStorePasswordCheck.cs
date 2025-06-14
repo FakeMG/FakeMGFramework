faf127f0cb90665d688bed32fe1bb7e1f00736d8
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2025-06-14 11:46:52 +0700

    feat: Android KeyStorePasswordCheck

---
+++ Editor/KeyStorePasswordCheck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace FakeMG.FakeMGFramework.Editor {
+    public class KeyStorePasswordCheck : IPreprocessBuildWithReport {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report) {
+#if UNITY_ANDROID
+        if (string.IsNullOrEmpty(PlayerSettings.Android.keystorePass))
+        {
+            Debug.LogError("Build canceled: Android KeyStore password is not set.");
+            throw new BuildFailedException("Android KeyStore password is not set.");
+        }
+
+        if (string.IsNullOrEmpty(PlayerSettings.Android.keyaliasPass))
+        {
+            Debug.LogError("Build canceled: Key Alias password is not set.");
+            throw new BuildFailedException("Key Alias password is not set.");
+        }
+#endif
+        }
+    }
+}
