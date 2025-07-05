a7e7bc1dcbaaea03ad4980b2cb27e1e9f09f354c
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2025-07-05 20:57:18 +0700

    feat: add ScrollRect extension for scrolling to target

---
+++ ExtensionMethods/ScrollRectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FakeMG.FakeMGFramework.ExtensionMethods
+{
+    public static class ScrollRectExtension
+    {
+        public static void ScrollTo(this ScrollRect scrollRect, Transform target, bool isVertical = true)
+        {
+            scrollRect.normalizedPosition = isVertical
+                ? new Vector2(0f, 1f - (scrollRect.content.rect.height / 2f - target.localPosition.y) / scrollRect.content.rect.height)
+                : new Vector2(1f - (scrollRect.content.rect.width / 2f - target.localPosition.x) / scrollRect.content.rect.width, 0f);
+        }
+    }
+}
