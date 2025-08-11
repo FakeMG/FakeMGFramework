af87decbddeb458ac968d783581009d8fa8de2f2
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2025-08-11 16:17:06 +0700

    feat: gacha

---
+++ Gacha/GachaReward.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FakeMG.FakeMGFramework.Gacha
+{
+    [Serializable]
+    public class GachaRewardData
+    {
+        public ItemBaseSO rewardObject;
+        public float probability;
+        public int amount = 1;
+    }
+}
---
+++ Gacha/GachaSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FakeMG.FakeMGFramework.Gacha
+{
+    public class GachaSystem : MonoBehaviour
+    {
+        [SerializeField, ValidateInput("ValidateProbabilities", "All probabilities must add up to 1.0")]
+        private List<GachaRewardData> rewards;
+
+        private bool ValidateProbabilities()
+        {
+            if (rewards == null || rewards.Count == 0) return true;
+
+            float totalProbability = rewards.Sum(reward => reward.probability);
+            return Mathf.Approximately(totalProbability, 1f);
+        }
+
+        public int ChooseRandomReward()
+        {
+            float randomValue = Random.value;
+            float cumulativeProbability = 0f;
+
+            for (int i = 0; i < rewards.Count; i++)
+            {
+                cumulativeProbability += rewards[i].probability;
+                if (randomValue <= cumulativeProbability)
+                {
+                    Debug.Log($"Chosen reward index: {i}, Name: {rewards[i].rewardObject.ItemName}");
+                    return i;
+                }
+            }
+
+            // Fallback in case of rounding errors
+            return rewards.Count - 1;
+        }
+    }
+}
