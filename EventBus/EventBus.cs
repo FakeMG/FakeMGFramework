b4d138f69fbe7f23c34f106760f886ec51cf2181
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2025-08-22 16:23:39 +0700

    feat: add EventBus

---
+++ EventBus/EventBus.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FakeMG.FakeMGFramework.EventBus
+{
+    public static class EventBus<T> where T : IEvent
+    {
+        public static event Action<T> OnEvent;
+        public static event Action OnEventNoArgs;
+
+        public static void Raise(T evt)
+        {
+            OnEvent?.Invoke(evt);
+            OnEventNoArgs?.Invoke();
+        }
+    }
+}
---
+++ EventBus/Events.cs
@@ -0,0 +1,12 @@
+﻿namespace FakeMG.FakeMGFramework.EventBus
+{
+    public interface IEvent { }
+
+    public struct TestEvent : IEvent { }
+
+    public struct PlayerEvent : IEvent
+    {
+        public int Health;
+        public int Mana;
+    }
+}
