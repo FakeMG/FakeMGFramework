af0c3c177bed5b9a97a0bc95cfe7618372610049
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2025-09-04 21:46:06 +0700

    feat: SO reference

---
+++ SOReference.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace FakeMG.FakeMGFramework
+{
+    public abstract class SOReference<T> : ScriptableObject where T : Object
+    {
+        public T Value { get; private set; }
+
+        public void Set(T newValue) => Value = newValue;
+        public bool IsAssigned => Value;
+    }
+}
