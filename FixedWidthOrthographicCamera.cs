7e1b654328cd08072e66e3f92b2d334927a2196b
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2026-03-16 09:14:23 +0700

    fix: get camera through field instead of Camera.main

--- FixedWidthOrthographicCamera.cs
+++ FixedWidthOrthographicCamera.cs
@@ -5,22 +5,21 @@ namespace FakeMG.Framework
 {
     public class FixedWidthOrthographicCamera : MonoBehaviour
     {
+        [SerializeField] private Camera _mainCamera;
         [Tooltip("The width of your level in Unity World Units that must always be visible.")]
         [SerializeField] private float _targetWidth = 10f;
 
         private void Awake()
         {
             // Orthographic size is half the vertical size of the camera view in world units.
-            Camera cam = Camera.main;
-            cam.orthographicSize = (_targetWidth / cam.aspect) * 0.5f;
+            _mainCamera.orthographicSize = (_targetWidth / _mainCamera.aspect) * 0.5f;
         }
 
         [Button]
         private void SetTargetWidthToCurrentWidth()
         {
-            Camera cam = Camera.main;
-            _targetWidth = cam.orthographicSize * 2 * cam.aspect;
-            cam.orthographicSize = _targetWidth / cam.aspect * 0.5f;
+            _targetWidth = _mainCamera.orthographicSize * 2 * _mainCamera.aspect;
+            _mainCamera.orthographicSize = _targetWidth / _mainCamera.aspect * 0.5f;
         }
     }
 }
